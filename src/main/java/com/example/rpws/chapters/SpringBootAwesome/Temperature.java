da5bb72663e16239c58146497392bf38db29e47b
Luffy <need4spd@naver.com>
2019-11-23 00:08:41 +0900

    Add temperature class

---
+++ src/main/java/com/example/rpws/chapters/SpringBootAwesome/Temperature.java
@@ -0,0 +1,14 @@
+package com.example.rpws.chapters.SpringBootAwesome;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public final class Temperature {
+
+    private final double value;
+
+}
